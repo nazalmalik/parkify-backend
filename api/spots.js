901d1e4ae88699fe89245a1e62d3d8b1f62d76e8
Nazal_Malik <155678924+nazalmalik@users.noreply.github.com>
2025-07-25 10:18:53 +0500

    Delete api/spots.js

--- api/spots.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const vehicleType = req.query.vehicleType;
-    // get spots logic
-    res.status(200).json({ spots: [] });
-  }
-}
